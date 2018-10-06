04ad8f4e67cc8a771b2232a43f0e1c857c585377
kseebrinegar <ksee.brinegar@hotmail.com>
2018-10-06 16:29:35 -0700

    just finished the heading for about page

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -2,7 +2,25 @@ import * as React from "react";
 
 class About extends React.Component {
     public render() {
-        return <main className="About" />;
+        return (
+            <main className="about">
+                <div className="about-intro">
+                    <h2>
+                        This project is a showcase of
+                        <span> influential female designers</span> who live and
+                        work in Ireland.
+                    </h2>
+
+                    <p className="para-default">
+                        Today the Irish design sector is 25% Female to 75% Male
+                        (Policy Framework, 2016). This ratio is simply not good
+                        enough. Gender balance was the last item on the last
+                        page of the Policy framework for Design Enterprise in
+                        Ireland. It’s time to change this.
+                    </p>
+                </div>
+            </main>
+        );
     }
 }
 
